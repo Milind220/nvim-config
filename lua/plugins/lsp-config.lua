e0badbb33be99327486020e74250cd457f04d065
Milind Sharma <milindsharma8@gmail.com>
2024-01-20 17:27:00 +0800

    Add LSP powered snippet completion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,10 +29,14 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+            -- This next line sets up the LSPs so that they have their stuff setup to allow for cmp-nvim-lsp to work
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 			-- Language servers that I require
 			-- customised settings can go here
-			lspconfig.lua_ls.setup({})
+			lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
 			lspconfig.arduino_language_server.setup({
 				cmd = {
 					"arduino-language-server",
@@ -45,11 +49,20 @@ return {
 					"-fqbn",
 					"arduino:avr:uno",
 				},
+                capabilities = capabilities
 			})
-			lspconfig.clangd.setup({})
-			lspconfig.marksman.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.rust_analyzer.setup({})
+			lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
+			lspconfig.marksman.setup({
+                capabilities = capabilities
+            })
+			lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
+			lspconfig.rust_analyzer.setup({
+                capabilities = capabilities
+            })
 
 			-- language server related keybindings
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})

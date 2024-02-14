6f3cb240eac54e0fb64733f99d7bcc0622bdeb8e
Milind Sharma <milindsharma8@gmail.com>
2024-02-15 01:03:36 +0800

    Add linenumbers in nvim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,6 +14,7 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
+                    "json-ls",
 					"lua_ls",
 					"arduino_language_server",
 					"clangd",
